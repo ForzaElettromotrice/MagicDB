cab999f00f7f91e5f2e1db8064706a2317cd619a
ForzaElettromotrice <mingardi.federico@gmail.com>
2025-04-10 17:46:32 +0200

    aggiornato DB generator

--- DBGenerator.sql
+++ DBGenerator.sql
@@ -1,44 +1,24 @@
 create sequence active_ability_id_seq
     as integer;
 
-alter sequence active_ability_id_seq owner to f3m;
-
 create type colors_t as enum ('Red', 'Green', 'Blue', 'White', 'Black');
-
-alter type colors_t owner to f3m;
 
 create type types_t as enum ('Artifact', 'Creature', 'Enchantment', 'Instant', 'Land', 'Planeswalker', 'Kindred', 'Tribal', 'Sorcery', 'World');
 
-alter type types_t owner to f3m;
-
 create type supertypes_t as enum ('Base', 'Snow', 'Legendary');
-
-alter type supertypes_t owner to f3m;
 
 create type subtypes_t as enum ('Otter', 'Gobakhan', 'Omenpath', 'Chicken', 'Possum', 'Sloth', 'Domri', 'Hamster', 'sECreT', 'Donkey', 'Art', 'Aurochs', 'Homunculus', 'Aetherborn', 'Walrus', 'Ikoria', 'Mountain', 'Slug', 'Spy', 'Tasha', 'Brushwagg', 'Gremlin', 'Hag', 'Mercadia', 'Fire', 'Will', 'Fungus', 'Arkhos', 'Cave', 'Spuzzem', 'Beast', 'Daretti', 'Elder', 'Devil', 'Quintorius', 'Rabbit', 'Surrakar', 'Brainiac', 'Elves', 'Serra’s Realm', 'Kinshala', 'Fractal', 'Champion', 'Ashiok', 'Townsfolk', 'Cartouche', 'Darillium', 'Mirrodin', 'Gus', 'Advisor', 'Ape', 'Lamia', 'Nautilus', 'Kaldheim', 'Elk', 'Szat', 'Lesson', 'Narset', 'Coyote', 'Dryad', 'Sarkhan', 'Homarid', 'Kylem', 'Mount', 'Nahiri', 'Nightstalker', 'Tree', 'Pyrulea', 'Theros', 'Ouphe', 'Bloomburrow', 'Ir', 'Spike', 'Shark', 'Chandra', 'Toy', 'Etiquette', 'Kangaroo', 'Davriel', 'Moonfolk', 'Frog', 'Mole', 'Human?', 'Hyena', 'Drone', 'Giant', 'Bard', 'Liliana', 'Antausia', 'Master', 'Sphinx', 'Leviathan', 'Nymph', 'Saheeli', 'Bolas', 'Deb', 'Zendikar', 'Eldraine', 'Zhalfir', 'Elminster', 'Wanderer', 'Mission', 'Shrine', 'Kandoka', 'Swamp', 'Ravnica', 'Alara', 'Tiefling', 'Mite', 'Kobold', 'Lhurgoyf', 'Fabacin', 'Ally', 'Contraption', 'Moon', 'Byode', 'Svega', 'Dog', 'New Earth', 'Room', 'Siege', 'Mouse', 'Snake', 'Hawk', 'Minotaur', 'Iquatana', 'Kraken', 'Deer', 'Pirate', 'Alien', 'Huatli', 'Antelope', 'Bringer', 'Fiora', 'Robot', 'Hatificer', 'Thunder Junction', 'Mongseng', 'Clown', 'Dragon', 'Tyranid', 'Snail', 'Jared', 'Shade', 'Teyo', 'Rowan', 'Artist', 'Ferret', 'Armadillo', 'Monger', 'Yanling', 'Vampire', 'Chameleon', 'Urza', 'Jellyfish', 'Vronos', 'Teferi', 'Elf', 'Beaver', 'Estrid', 'Jaya', 'Class', 'Salamander', 'Valla', 'Serra', 'Unicorn', 'Masticore', 'Fortification', 'Efreet', 'Grist', 'Cat', 'Praetor', 'Bureaucrat', 'Kor', 'Archon', 'Oko', 'Hydra', 'Mystic', 'Pegasus', 'Ugin', 'Cyborg', 'Rath', 'Segovia', 'Rhino', 'Trenzalore', 'Point', 'Lobster', 'Dovin', 'Gnoll', 'Urza''s', 'Glimmer', 'Curse', 'and/or', 'Alicorn', 'Wraith', 'Elephant', 'Pilot', 'Athlete', 'Gorgon', 'Adventure', 'Citizen', 'Thrull', 'Licid', 'Gnome', 'Plains', 'Dalek', 'Scarecrow', 'Splinter', 'Pangolin', 'Shapeshifter', 'Carrier', 'Food', 'Assembly-Worker', 'Turtle', 'Yanggu', 'Plant', 'Phyrexian', 'Centaur', 'Autobot', 'Serpent', 'Seal', 'Octopus', 'Abian', 'Performer', 'Synth', 'Ulamog''s', 'Warrior', 'of', 'Eldrazi', 'Karn', 'Drake', 'Apalapucia', 'Arlinn', 'Weasel', 'Shadowmoor', 'Moag', 'Cephalid', 'Nixilis', 'Merfolk', 'Cyclops', 'Slith', 'Druid', 'Yeti', 'Skeleton', 'Kithkin', 'Sorin', 'Gate', 'Aminatou', 'Monkey', 'Basri', 'Specter', 'Kirin', 'Tarkir', 'Vryn', 'Lammasu', 'Wildfire', 'Belenon', 'Case', 'Dungeon', 'Rigger', 'Nastiest,', 'Peasant', 'Egg', 'Paratrooper', 'Phoenix', 'Satyr', 'Ersta', 'Gallifrey', 'Jackal', 'Sable', 'Scientist', 'The Abyss', 'Nomad', 'Djinn', 'The Library', 'Capenna', 'Shenmeng', 'Soldier', 'Mutant', 'Calix', 'Treasure', 'Karsus', 'Imp', 'Town', 'Lizard', 'Mammoth', 'Ranger', 'Gamer', 'Dauthi', 'Whale', 'Desert', 'Vampyre', 'Equipment', 'Varmint', 'Fox', 'Astartes', 'Zombie', 'Cridhe', 'Innistrad', 'Cow', 'Kaya', 'Androzani Minor', 'Lorwyn', 'Aura', 'Shaman', 'Reflection', 'Control', 'Spider', 'Time', 'Nephilim', 'New Phyrexia', 'Xerex', 'Myr', 'Wrestler', 'Trap', 'Bobblehead', 'Juggernaut', 'Power-Plant', 'Custodes', 'Saga', 'Bird', 'Dwarf', 'Capybara', 'Thopter', 'Demigod', 'Beeble', 'Pony', 'Venser', 'Zariel', 'Kaito', 'Construct', 'Halfling', 'Incarnation', 'Arcavios', 'Squirrel', 'Azra', 'Garruk', 'Las Vegas', 'Time Lord', 'Golem', 'Judge', 'Kephalai', 'Coward', 'Pest', 'Sponge', 'Phelddagrif', 'Realm', 'Sphere', 'Hero', 'Treefolk', 'Rat', 'Survivor', 'Skunk', 'Hellion', 'Zonian', 'Necron', 'Shandalar', 'Guff', 'Trilobite', 'Attraction', 'Camel', 'Mars', 'Horror', 'Avatar', 'Lukka', 'Bolas''s Meditation Realm', 'Locus', 'Azgol', 'Crocodile', 'Lady', 'Comet', 'Waiter', 'Metathran', 'Ral', 'Raccoon', 'Horse', 'Assassin', 'Volver', 'Elspeth', 'You', 'Dominaria', 'LaIR', 'Omen', 'Designer', 'Thalakos', 'Minion', 'Baddest,', 'Cloud', 'Bear', 'Tezzeret', 'Chimera', 'Gargantikar', 'Angrath', 'Cleric', 'The', 'Hippo', 'Rabiah', 'Jace', 'Zubera', 'Ogre', 'Kolbahan', 'Spawn', 'Vivien', 'Warlock', 'Dreadnought', 'Freyalise', 'Phyrexia', 'Employee', 'Illusion', 'Worm', 'Blind Eternities', 'Inquisitor', 'Ship', 'Gideon', 'Monk', 'Scorpion', 'Villain', 'Tamiyo', 'Beholder', 'Basilisk', 'Kyneth', 'Unknown Planet', 'Wurm', 'Xenagos', 'Goblin', 'Elemental', 'Avishkar', 'Mine', 'Guest', 'Dihada', 'Noggle', 'C''tan', 'Processor', 'Cockatrice', 'Crab', 'Fish', 'Sliver', 'Badger', 'Kamigawa', 'Clamfolk', 'Arcane', 'Forest', 'Gith', 'Orc', 'Knight', 'Sivitri', 'Saproling', 'Chef', 'Demon', 'Vehicle', 'Griffin', 'Biggest,', 'Chicago', 'Vedalken', 'Tower', 'Tibalt', 'Boxer', 'Berserker', 'Duck', 'Powerstone', 'Amsterdam', 'Outside Mutter''s Spiral', 'Necros', 'Mercenary', 'Regatha', 'Samut', 'Human', 'Universia Beyondia', 'Symbiote', 'Proper', 'Troll', 'Orgg', 'Squid', 'Noble', 'Head', 'Scout', 'Chorus', 'Harpy', 'Ooze', 'Igpay', 'Wizard', 'Hippogriff', 'Mordenkainen', 'Primarch', 'Gargoyle', 'Penguin', 'Amonkhet', 'Elemental?', 'Skaro', 'Key', 'MagicCon', 'Kavu', 'Dack', 'Jeska', 'Wrenn', 'Inzerva', 'Rogue', 'Ixalan', 'Kiora', 'Mongoose', 'Equilor', 'Barbarian', 'Quest', 'Samurai', 'Alfava Metraxis', 'Goat', 'Flagbearer', 'Detective', 'Boar', 'Moogle', 'The Dalek Asylum', 'Duskmourn', 'Luvion', 'Kasmina', 'Nissa', 'Vraska', 'Foldaria', 'Tyvar', 'Siren', 'Foundations', 'Lair', 'Ajani', 'Doctor', 'Clamhattan', 'Faerie', 'Spellshaper', 'Island', 'Manticore', 'Echoir', 'Wolf', 'Eye', 'Windgrace', 'Starfish', 'Secret Lair', 'Insect', 'Secret', 'Horsehead Nebula', 'Wall', 'Ellywick', 'Luxior', 'Background', 'Armored', 'Mime', 'Child', 'God', 'Ox', 'Rebel', 'Hell', 'Weird', 'Porcupine', 'Wombat', 'B.O.B.', 'Artificer', 'Oyster', 'Ergamon', 'Mummy', 'Niko', 'Spirit', 'Atog', 'Ulgrotha', 'Clue', 'Bat', 'Nightmare', 'Ninja', 'Spacecraft', 'Minsc', 'Earth', 'Soltari', 'Bahamut', 'Werewolf', 'Muraganda', 'Lolth', 'Cyberman', 'Killbot', 'Grandchild', 'Koth', 'Wolverine', 'Rune', 'Sheep', 'Leech', 'Angel', 'Dinosaur', 'Dakkon', 'Archer');
 
-alter type subtypes_t owner to f3m;
-
 create type constant_characteristics_t as enum ('Hexproof', 'Defender', 'Flash', 'Reach', 'Haste', 'Deathtouch', 'Fear', 'Land-walk', 'Protection', 'Shroud', 'Flying', 'Affinity', 'Absorb', 'Banding', 'Changeling', 'Horsemanship', 'Chroma', 'Hellbent', 'Domain', 'Epic', 'Vanishing', 'Phasing', 'Graft', 'Intimidate', 'Shadow', 'Radiance', 'Threshold', 'Delve', 'Sunburst', 'Split Second', 'Modular', 'Ward');
-
-alter type constant_characteristics_t owner to f3m;
 
 create type attack_characteristics_t as enum ('First strike', 'Vigilance', 'Double strike', 'Lifelink', 'Annihilator', 'Trample', 'Poisonous', 'Wither', 'Exalted', 'Frenzy', 'Rampage', 'Battle cry', 'Provoke', 'Clash');
 
-alter type attack_characteristics_t owner to f3m;
-
 create type activable_characteristics_t as enum ('Entwine', 'FlashbackEvoke', 'Equip', 'Regeneration', 'Cycling', 'Fortify', 'Channel', 'Morph', 'Multikicker', 'Offering', 'Kicker', 'Forecast', 'Retrace', 'Reinforce', 'Buyback', 'Grandeur', 'Suspend', 'Transfigure', 'Transmute', 'Splice onto Arcane', 'Cumulative upkeep', 'Echo', 'Unearth', 'Aura swap', 'Sweep', 'Madness', 'Conspire', 'Dredge', 'Replicate');
-
-alter type activable_characteristics_t owner to f3m;
 
 create type triggerable_characteristics_t as enum ('Suspend', 'Graft', 'Epic', 'Vanishing', 'Madness', 'Kinship', 'Imprint', 'Flanking', 'Soulbound', 'Totem Armor', 'Champion', 'Cascade', 'Convoke', 'Conspire', 'Fateseal', 'Devour', 'Undying', 'Modular', 'Soulshift', 'Persist', 'Scry', 'Ripple', 'Recover', 'Replicate', 'Bloodthirst', 'Storm', 'Gravestorm', 'Haunt', 'Landfall', 'Hideaway', 'Amplify', 'Fading');
 
-alter type triggerable_characteristics_t owner to f3m;
-
 create domain uint as integer
     constraint positive check (VALUE >= 0);
-
-alter domain uint owner to f3m;
 
 create table mana_cost
 (
@@ -56,9 +36,6 @@ create table mana_cost
         unique (colorless, red, blue, green, black, white, snow)
 );
 
-alter table mana_cost
-    owner to f3m;
-
 create table card
 (
     name      varchar not null
@@ -73,9 +50,6 @@ create table card
     loyalty   uint
 );
 
-alter table card
-    owner to f3m;
-
 create table types
 (
     name varchar not null
@@ -86,9 +60,6 @@ create table types
     constraint types_pk
         primary key (name, type)
 );
-
-alter table types
-    owner to f3m;
 
 create table supertypes
 (
@@ -101,9 +72,6 @@ create table supertypes
         primary key (name, supertype)
 );
 
-alter table supertypes
-    owner to f3m;
-
 create table subtypes
 (
     name    varchar    not null
@@ -114,9 +82,6 @@ create table subtypes
     constraint subtypes_pk
         primary key (name, subtype)
 );
-
-alter table subtypes
-    owner to f3m;
 
 create table colors
 (
@@ -129,9 +94,6 @@ create table colors
         primary key (name, color)
 );
 
-alter table colors
-    owner to f3m;
-
 create table constant_characteristics
 (
     name           varchar                    not null
@@ -142,9 +104,6 @@ create table constant_characteristics
     constraint constant_characteristic_pk
         primary key (name, characteristic)
 );
-
-alter table constant_characteristics
-    owner to f3m;
 
 create table attack_characteristics
 (
@@ -157,9 +116,6 @@ create table attack_characteristics
         primary key (name, characteristic)
 );
 
-alter table attack_characteristics
-    owner to f3m;
-
 create table triggerable_characteristics
 (
     name           varchar                       not null
@@ -169,9 +125,6 @@ create table triggerable_characteristics
     constraint triggerable_characteristics_pk
         primary key (name, characteristic)
 );
-
-alter table triggerable_characteristics
-    owner to f3m;
 
 create table activable_characteristics
 (
@@ -183,9 +136,6 @@ create table activable_characteristics
     constraint activable_characteristics_pk
         primary key (name, characteristic)
 );
-
-alter table activable_characteristics
-    owner to f3m;
 
 create table activable_ability
 (
@@ -208,9 +158,6 @@ create table activable_ability
         unique (name, loyalty, life_points, sacrifice, mana_cost, tap, discard)
 );
 
-alter table activable_ability
-    owner to f3m;
-
 alter sequence active_ability_id_seq owned by activable_ability.id;
 
 create table double_face_card
@@ -229,9 +176,6 @@ create table double_face_card
     isfront boolean not null
 );
 
-alter table double_face_card
-    owner to f3m;
-
 create table double_card
 (
     name    varchar not null
@@ -245,9 +189,6 @@ create table double_card
         constraint double_card_fk_2
             references card
 );
-
-alter table double_card
-    owner to f3m;
 
 create table meld_card
 (
@@ -263,9 +204,6 @@ create table meld_card
         constraint meld_card_fk_3
             references card
 );
-
-alter table meld_card
-    owner to f3m;
 
 create function bidirectionaldoublefacecard() returns trigger
     language plpgsql
@@ -294,8 +232,6 @@ BEGIN
 END;
 $$;
 
-alter function bidirectionaldoublefacecard() owner to f3m;
-
 create trigger triggerdoublefacecard
     after insert
     on double_face_card
@@ -316,8 +252,6 @@ BEGIN
     RETURN NEW;
 END;
 $$;
-
-alter function bidirectionaldoublecard() owner to f3m;
 
 create function bidirectionalmeldcard() returns trigger
     language plpgsql
@@ -351,8 +285,6 @@ BEGIN
 END;
 $$;
 
-alter function bidirectionalmeldcard() owner to f3m;
-
 create function checkplaneswalkerintegrity() returns trigger
     language plpgsql
 as
@@ -370,8 +302,6 @@ BEGIN
 END;
 $$;
 
-alter function checkplaneswalkerintegrity() owner to f3m;
-
 create function checkisplaneswalker() returns trigger
     language plpgsql
 as
@@ -388,8 +318,6 @@ BEGIN
     RETURN NEW;
 END;
 $$;
-
-alter function checkisplaneswalker() owner to f3m;
 
 create trigger triggeractivableabilityplaneswalker
     after insert
