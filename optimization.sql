c9d7d5c308fbddb9e61bc6375d8599177d674a86
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-11 12:58:02 +0200

    create index on tot_mana

--- optimization.sql
+++ optimization.sql
@@ -5,6 +5,7 @@ CREATE INDEX index_attack ON card(power);
 CREATE INDEX index_defense ON card(defense);
 CREATE INDEX index_mana ON card(mana_cost);
 CREATE INDEX index_type ON types(type);
+CREATE UNIQUE INDEX tot_mana_pk ON tot_mana(id, cost)
 --To optimize the DB, create this materialized view
 CREATE MATERIALIZED VIEW tot_mana AS
     SELECT m.red+m.black+m.white+m.snow+m.green+m.blue+m.colorless AS cost,
