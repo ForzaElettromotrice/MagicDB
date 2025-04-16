b6c4a5d808ad5eaca9f269278ad926fb17120d3e
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-17 00:00:45 +0200

    Update query2_optimized.sql

--- query2_optimized.sql
+++ query2_optimized.sql
@@ -1,4 +1,5 @@
 -- 2. Find the number of creatures with Flying with a power of at least 6
+-- We can optimize this query by not using the distinct clause since there are no duplicates and using inner joins to avoid too many 'where' clauses
 EXPLAIN ANALYSE
 SELECT count(c.name) AS total
 FROM card c
