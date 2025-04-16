c1638592f6050824f27103cb73ce25a0571e9eea
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-17 00:11:27 +0200

    Update query8_optimized.sql

--- query8_optimized.sql
+++ query8_optimized.sql
@@ -1,4 +1,5 @@
 --8. Find instants with a cost of exactly 1 colored mana grouped by color
+-- We can optimize this query by only selecting the fields we are interested in; not using 'distinct' since there are no duplicates; using the view to calculate the total mana cost and inner joins instead of multiple 'where' clauses
 EXPLAIN ANALYSE
 SELECT cl.color, c.name
 FROM card c
