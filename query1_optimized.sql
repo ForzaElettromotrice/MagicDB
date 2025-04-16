a3182a744c09f187bd7080fd38cc1562eb822fdc
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-16 23:39:24 +0200

    Update query1_optimized.sql

--- query1_optimized.sql
+++ query1_optimized.sql
@@ -1,5 +1,5 @@
 -- 1. Find all legendary Planeswalkers that have loyalty abilities with a minimum cost of -5 and a maximum cost of +1
--- To optimize this query, we can remove the Card table since the name can be inferred by the type; we use inner joins to avoid too many 'where' conditions; and we use the 'between' operator instead of two separate checks
+-- To optimize this query, we can remove the Card table since the name can be inferred by the type; we only select the fields we are interested in; we use inner joins to avoid too many 'where' conditions; and we use the 'between' operator instead of two separate checks
 EXPLAIN ANALYSE -- This command is used to examine the efficiency of the query 
 SELECT DISTINCT t.name
 FROM types t
