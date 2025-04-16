067dcc7ced84a1ebbfeda88396649ab0c8125cdc
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-16 23:40:36 +0200

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,5 +1,6 @@
 -- 2. Find the number of creatures with Flying with a power of at least 6
-EXPLAIN ANALYSE
+-- First, we select cards according to type (Creature) and characteristic (Flying), then we check if the creature has a power higher than 5 and we count them
+EXPLAIN ANALYSE -- This command is used to examine the efficiency of the query 
 SELECT count(DISTINCT c.name) AS total
 FROM card c, constant_characteristics cc, types t
 WHERE c.name=cc.name
