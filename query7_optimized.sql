2c6420ebfe09e6e0c34b8532043a58da6d7fa6de
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-17 00:10:03 +0200

    Update query7_optimized.sql

--- query7_optimized.sql
+++ query7_optimized.sql
@@ -1,4 +1,5 @@
 --7. Find all the cards with a defense of at least 2 that have abilities requiring the sacrifice of a creature
+-- This query can be optimized by only selecting the fields we are interested in; not using 'distinct' since there are no duplicates; using inner joins insteadof multiple 'where' clauses
 EXPLAIN ANALYSE
 SELECT c.name, c.defense
 FROM card c
