b79ce5f2d762589bb0689124581a99008b62bd80
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-16 23:57:28 +0200

    Update query12.sql

--- query12.sql
+++ query12.sql
@@ -1,6 +1,5 @@
 -- 12. Find dual lands that gives you at least 1 blue mana
 -- First, we select cards according to type (Land), then we count the colors of each land card and only select those that have two; lastly we check if at least one of the two colors is blue with the 'Exists' operator
--- Distinct is used to avoid duplicates
 EXPLAIN ANALYSE -- This command is used to examine the efficiency of the query 
 SELECT c.*
 FROM card c, types t
