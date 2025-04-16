1d3b1a391aa930739d068483f36ce450a5576532
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-16 23:32:25 +0200

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,5 +1,7 @@
 -- 1. Find all legendary Planeswalkers that have loyalty abilities with a minimum cost of -5 and a maximum cost of +1
-EXPLAIN ANALYSE
+-- First, we select cards according to type (Planeswalker) and supertype (Legendary), then we select the correct loyalty value the creature must have
+-- Distinct is used to avoid duplicates
+EXPLAIN ANALYSE -- This command is used to examine the efficiency of the query 
 SELECT DISTINCT c.*
 FROM card c, types t, supertypes st, activable_ability a
 WHERE c.name=t.name
