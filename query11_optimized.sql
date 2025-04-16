c2c12b88ec0be30fd2a8659c1a22fd0ace284959
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-16 23:56:42 +0200

    Update query11_optimized.sql

--- query11_optimized.sql
+++ query11_optimized.sql
@@ -1,4 +1,5 @@
 -- 11. Find all meld cards of which you are the second
+-- We can optimize this query by selecting only the fields we are interested in; not using distinct since there are no duplicates; and doing an inner join to avoid 'where' conditions
 EXPLAIN ANALYSE
 SELECT c.name
 FROM card c
