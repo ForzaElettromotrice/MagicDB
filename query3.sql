710416e8fff822ea0384b4bb1c7ae8d694906b8b
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-17 00:01:19 +0200

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -1,6 +1,5 @@
 -- 3. Find the average defense of creatures with Deathtouch but without Reach
 -- First, we select cards with Deathtouch; then, to avoid those with Reach, we do a left join: this way, if the creature does not have Reach, its name will be null, thus we can select it. In the end, we calculate the average defence
--- Distinct is used to avoid duplicates
 EXPLAIN ANALYSE -- This command is used to examine the efficiency of the query 
 SELECT AVG(c.defense) as avg_defense
 FROM card c
