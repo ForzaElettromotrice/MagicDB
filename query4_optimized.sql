fe39b412d7740517bbde80d81c737be716c52bc5
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-17 00:04:12 +0200

    Update query4_optimized.sql

--- query4_optimized.sql
+++ query4_optimized.sql
@@ -1,4 +1,5 @@
 -- 4. Find all Red\Black enchantments with a 6 Mana Cost
+-- We can optimize this query by selecting only the fields we are interested in; using a inner join to avoid too many 'where' clauses; using the view to calculate the total mana cost instead of computing it each tim
 EXPLAIN ANALYSE
 SELECT DISTINCT c.name
 FROM card c
