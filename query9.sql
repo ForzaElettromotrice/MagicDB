0a34a16ac0a10d4f9584c4e14d9801719e12cf3d
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-17 00:11:55 +0200

    Update query9.sql

--- query9.sql
+++ query9.sql
@@ -1,5 +1,6 @@
 -- 9. Find all creatures with power higher than defense, green with Trample, Vigilance and cost >= 3
 -- First, we select cards according to type (Creature) and characteristics (Trample and Vigilance), then we check if one of the colors is green, calculate the total mana cost and check if it's higher than 2
+-- Distinct is used to avoid duplicates
 EXPLAIN ANALYSE -- This command is used to examine the efficiency of the query 
 SELECT DISTINCT c.*
 FROM card c, types t, colors cl, attack_characteristics ac1, attack_characteristics ac2
