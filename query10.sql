04a317b0fc6e9ecf6d459a7b1b8580c3df9821ba
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-16 23:36:16 +0200

    Update query10.sql

--- query10.sql
+++ query10.sql
@@ -1,5 +1,7 @@
 -- 10. Find all the creatures with Champion ability ordered by power
-EXPLAIN ANALYSE
+-- First, we select cards according to type (Creature) and characteristic (Champion), we order according to power, with the most powerful on top
+-- Distinct is used to avoid duplicates
+EXPLAIN ANALYSE -- This command is used to examine the efficiency of the query 
 SELECT DISTINCT c.*
 FROM card c, triggerable_characteristics tc, types t
 WHERE c.name = tc.name
