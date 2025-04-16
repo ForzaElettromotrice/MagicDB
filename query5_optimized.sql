cb2af30e86ae4d31f180e90cc037bad9275f81b2
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-17 00:06:44 +0200

    Update query5_optimized.sql

--- query5_optimized.sql
+++ query5_optimized.sql
@@ -1,4 +1,5 @@
 -- 5. Find the equippable artifact(s) with the highest mana cost
+-- We can optimize this query by calculating the total mana cost with the view; using a inner join instead of multiple 'where' clauses; and instead of doing a nested query we can order by higher mana cost and just select the first element
 EXPLAIN ANALYSE
 SELECT c.name, m.tot as cost
 FROM card c
