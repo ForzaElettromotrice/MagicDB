2f5eee7ed8730dd0bb84deeef98a43713c4d8426
R0n3l3 <150672139+R0n3l3@users.noreply.github.com>
2025-04-16 23:59:07 +0200

    Update query12_optimized.sql

--- query12_optimized.sql
+++ query12_optimized.sql
@@ -1,5 +1,5 @@
 -- 12. Find dual lands that gives you at least 1 blue mana
--- 8. Find dual lands that gives you at least 1 blue mana
+-- We can optimize this query by selecting only the fields we are interested in; using an inner join to avoid too many 'where' conditions; not using the Card table since we can infer the name from the color; and counting distinct colors in the having clause instead of using a nested query
 EXPLAIN ANALYSE
 SELECT cl.name
 FROM colors cl
